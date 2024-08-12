9f25252ffbc018efa18e77d6f24f2efe83e8d9bb
0x2V <0x2V369@gmail.com>
2024-08-12 20:35:33 +0300

    Updated scoreboard logic when new level is reached

--- scoreboard.py
+++ scoreboard.py
@@ -8,21 +8,21 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.ht()
-        self.level = 0
+        self.level = 1
         self.penup()
         self.color("black")
         self.goto(-270, 260)
-        self.new_level()
+        self.update_score_board()
 
-    def next_level(self):
+    def update_score_board(self):
         """Print the correct level on the screen when game starts or new level is achieved"""
-        self.level += 1
         self.clear()
         self.write(f"Level: {self.level}", font=FONT)
 
     def new_level(self):
         """Update level text on the screen"""
-        self.next_level()
+        self.level += 1
+        self.update_score_board()
 
     def game_over(self):
         """Print the GAME OVER message in the middle of the screen"""
